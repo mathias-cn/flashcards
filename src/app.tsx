36bf59e4d75bd45fa9586175f0c928db3eaaf7e1
Mathias Navarre <mathiascnavarre@gmail.com>
2024-07-20 18:02:35 -0300

    Everything working

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
+import { Flashcard } from './components/flashcard'
 
 export function App() {
-  const [currentFlash, setCurrentFlash] = useState("")
+  const [currentFlash, setCurrentFlash] = useState(0)
+  const flashcards = [
+    { id: 1,question: "What language is React on?", answer: "Javascript" },
+    { id: 2,question: "What are the building blocks of React apps?", answer: "Components" },
+    { id: 3,question: "What's the name of the syntax we use to describe a UI in React?", answer: "JSX" },
+    { id: 4,question: "How to pass data from parent to child components?", answer: "Props" },
+    { id: 5,question: "How to give components memory?", answer: "useState hook" },
+    { id: 6,question: "What do we call an input element that is completely synchronised with state?", answer: "Controlled component" },
+  ]
 
   return (
-    <h1>Hello</h1>
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      {flashcards.map(f => (
+        currentFlash === f.id ? (
+          <Flashcard key={f.id} flashcardObj={f} clicked={true} />
+        ) : (
+          <Flashcard key={f.id} flashcardObj={f} clicked={false} onClick={() => setCurrentFlash(f.id)}  />
+        )
+      ))}
+    </div>
   )
 }
--- src/components/flashcard.tsx
+++ src/components/flashcard.tsx
@@ -1,5 +1,22 @@
-export function Flashcard() {
-    return (
-        <h1>asd</h1>
+interface FlashcardProps {
+    clicked: boolean
+    flashcardObj: {
+        id: number
+        question: string,
+        answer: string
+    }
+    onClick?: () => void
+}
+
+export function Flashcard({ clicked, flashcardObj, onClick }: FlashcardProps) {
+
+    return clicked ? (
+        <div className="bg-red-500 border-2 border-red-700 text-white rounded-md p-4 h-60 flex items-center justify-center text-center">
+            <p>{flashcardObj.answer}</p>
+        </div>
+    ) : (
+        <div onClick={onClick} className="bg-gray-200 border-2 border-gray-300 rounded-md p-4 h-60 flex items-center justify-center text-center cursor-pointer">
+            <p>{flashcardObj.question}</p>
+        </div>
     )
 }
